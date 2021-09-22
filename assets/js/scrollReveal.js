87a0a36bc3323aeaeaec3bed5b111b80262671c3
Marcus <marcus.sfreitas@outlook.com>
2021-09-21 23:31:56 -0300

    Adiciona animação de scroll

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+  reset: false,
+});
+
+ScrollReveal().reveal(".card", { delay: 200 });
+ScrollReveal().reveal(".card-data", { delay: 400 });
+ScrollReveal().reveal(".title", { delay: 500 });
+ScrollReveal().reveal(".description", { delay: 600 });
